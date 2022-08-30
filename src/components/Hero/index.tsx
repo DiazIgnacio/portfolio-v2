a677d1ceac27b082355b3bf31edbfc913e321b4b
Ignacio Diaz <diaz.ignacio023@gmail.com>
2022-08-30 05:07:53 -0300

    refactor: create Hero component

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,18 @@
+const Hero = () => {
+  return (
+    <div className="max-w-[648px]">
+      <h2 className="text-3xl font-normal leading-9">I'm Ignacio</h2>
+      <h1 className="mt-1 text-6xl font-semibold leading-[70px] text-[#0099FF]">
+        Front End Developer
+      </h1>
+      <p className="mt-5 text-xl leading-8">
+        Hi! I'm Ignacio Diaz, a Frontend Web Developer based in Argentina. I
+        have a passion for all things web, and love working on interactive and
+        responsive websites. In my free time, I like to read and spend time with
+        my family and friends.
+      </p>
+    </div>
+  )
+}
+
+export default Hero
