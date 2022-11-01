bf3d97c3cbcb28e57aa825ddf1322e50c7ec9ca5
Ignacio Diaz <diaz.ignacio023@gmail.com>
2022-11-01 13:56:14 -0300

    feat: create hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,16 +1,56 @@
+import Link from 'next/link'
+
 const Hero = () => {
   return (
-    <div className="max-w-[648px]">
-      <h2 className="text-3xl font-normal leading-9">I'm Ignacio</h2>
-      <h1 className="mt-1 text-6xl font-semibold leading-[70px] text-[#0099FF]">
-        Front End Developer
-      </h1>
-      <p className="mt-5 text-xl leading-8">
-        Hi! I'm Ignacio Diaz, a Frontend Web Developer based in Argentina. I
-        have a passion for all things web, and love working on interactive and
-        responsive websites. In my free time, I like to read and spend time with
-        my family and friends.
-      </p>
+    <div className="relative h-screen bg-black text-white">
+      <nav className="flex justify-between px-32 pt-6">
+        <Link href="/">
+          <div>
+            <span className="font-logo-first-letter text-5xl uppercase leading-10">
+              I
+            </span>
+            <span className="font-logo text-5xl uppercase leading-10">
+              gnacio
+            </span>
+          </div>
+        </Link>
+        <ul className="flex justify-end text-2xl font-normal leading-8">
+          <Link href="#about">
+            <li className="mr-8 cursor-pointer">About</li>
+          </Link>
+          <Link href="#projects">
+            <li className="mr-8 cursor-pointer">Projects</li>
+          </Link>
+          <Link href="#contact">
+            <li className="mr-8 cursor-pointer">Contact</li>
+          </Link>
+        </ul>
+      </nav>
+      <div className="mx-32 mt-24 w-4/5 max-w-[660px]">
+        <h2 className="text-3xl font-normal leading-9">I'm Ignacio</h2>
+        <h1 className="mt-1 text-6xl font-semibold leading-[70px] text-primary">
+          Front End Developer
+        </h1>
+        <p className="mt-5 text-xl leading-8">
+          Hi! I'm Ignacio Diaz, a Frontend Web Developer based in Argentina. I
+          have a passion for all things web, and love working on interactive and
+          responsive websites. In my free time, I like to read and spend time
+          with my family and friends.
+        </p>
+        <button className="relative z-20 mt-10 cursor-pointer rounded-2xl bg-gradient-to-t px-12 py-5 text-2xl font-medium leading-6">
+          Portfolio
+        </button>
+      </div>
+      <img
+        src="/images/wave-blurred.svg"
+        alt="wave"
+        className="absolute bottom-8"
+      />
+      <img
+        src="/images/wave-front.svg"
+        alt="wave"
+        className="absolute bottom-0 z-10"
+      />
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}'
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        white: '#f8f8f8',
+        black: '#151419',
+        primary: '#0099FF',
+        'primary-dark': '#007CCE',
+        gray: '#989898'
+      },
+      fontSize: {
+        '2xs': '.625rem', // 10px
+        xs: '.75rem', // 12px
+        sm: '.875rem', // 14px
+        base: '1rem', // 16px
+        lg: '1.125rem', // 18px
+        xl: '1.25rem', // 20px
+        '2xl': '1.5rem', // 24px
+        '3xl': '1.875rem', // 30px
+        '4xl': '2.25rem', // 36px
+        '5xl': '3rem', // 48px
+        '6xl': ' 3.75rem', // 60px
+        '7xl': '5rem' // 80px
+      },
+      // Font family
+      fontFamily: {
+        logo: ['Birthstone'],
+        'logo-first-letter': ['Bilbo Swash Caps']
+      },
+      // Gradient
+      backgroundImage: {
+        'gradient-to-t': 'linear-gradient(to top, #0099FF, #0074C2)' // Primary
+      }
+    }
   },
   plugins: []
-};
+}
