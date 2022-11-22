f52c1d4df4a04b8d40c241730bef8e611467073a
Ignacio Diaz <diaz.ignacio023@gmail.com>
2022-11-22 10:20:31 -0300

    feat: make background fixed

--- src/components/Statistics/Background.tsx
+++ src/components/Statistics/Background.tsx
@@ -1,11 +1,12 @@
 const Background = () => (
   <>
-    <img
-      src="https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80"
-      alt=""
-      className="absolute inset-0 -z-30 h-full w-full object-cover"
-    />
-
+    <div
+      className="absolute inset-0 -z-30 h-full bg-fixed"
+      style={{
+        backgroundImage:
+          'url("https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80")'
+      }}
+    ></div>
     <span className="absolute inset-0 -z-20 h-full w-full bg-black opacity-75"></span>
     <span className="absolute inset-0 -z-10 h-full w-full bg-primary opacity-10"></span>
   </>
