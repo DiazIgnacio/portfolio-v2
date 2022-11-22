2ed869ea7064d91894c4f5a573e66ccae6035b03
Ignacio Diaz <diaz.ignacio023@gmail.com>
2022-11-22 11:47:09 -0300

    style: fix body height

--- src/components/ScrollableBackground/index.tsx
+++ src/components/ScrollableBackground/index.tsx
@@ -1,7 +1,7 @@
 const ScrollableBackground = () => (
   <>
     <div
-      className="absolute inset-0 -z-30 h-full bg-fixed"
+      className="absolute inset-0 -z-30 overflow-hidden bg-fixed"
       style={{
         backgroundImage:
           'url("https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80")'
