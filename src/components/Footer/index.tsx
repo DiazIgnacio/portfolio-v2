f27248cbfc0e0530a2d8cb26d66d23b034cee461
Ignacio Diaz <diaz.ignacio023@gmail.com>
2023-05-11 00:22:35 -0300

    make footer button go to top

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,13 +2,17 @@ import Container from '../Container'
 import Logo from '../Logo'
 
 const Footer = (props: React.HTMLProps<HTMLDivElement>) => {
+  const goToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  }
+
   return (
     <Container
       className="space-y-8 md:flex md:items-center md:justify-between md:space-y-0"
       black
     >
       <div className="text-center md:w-1/6">
-        <Logo />
+        <Logo onClick={goToTop} />
       </div>
       <div className="md:w-2/6">
         <p className="text-center text-white md:text-left">
--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,16 +1,18 @@
-import { forwardRef } from 'react'
+import { HTMLAttributes, forwardRef } from 'react'
 
-const Logo = forwardRef<HTMLDivElement>((props, ref) => {
-  return (
-    <div
-      className="cursor-pointer text-4xl uppercase leading-10 md:text-5xl"
-      ref={ref}
-      {...props}
-    >
-      <span className="font-logo-first-letter">I</span>
-      <span className="font-logo">gnacio</span>
-    </div>
-  )
-})
+const Logo = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
+  (props, ref) => {
+    return (
+      <div
+        className="cursor-pointer text-4xl uppercase leading-10 md:text-5xl"
+        ref={ref}
+        {...props}
+      >
+        <span className="font-logo-first-letter">I</span>
+        <span className="font-logo">gnacio</span>
+      </div>
+    )
+  }
+)
 
 export default Logo
